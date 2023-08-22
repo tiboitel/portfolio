5c13dbf360ee309eb36137cc1cf3d4f66bdd20f2
tiboitel <tiboitel@student.42.fr>
2023-08-22 19:59:28 +0200

    feat: Add an easter egg on the console output.

--- src/ts/components/home.ts
+++ src/ts/components/home.ts
@@ -1,1 +1,8 @@
-console.log("Home.ts loaded");
+console.log("CONTACT ME:\n" +
+"----------\n\n" +
+"(\\__/)\n" +
+"(o^-^)\n" +
+"(_('')(')\n\n" +
+"Hello ! If you like this portfolio, leave a star on the Github repo: https://github.com/tiboitel/portfolio/\n\n" +
+"Don't hesitate to PM me at tiboitel@student.42.fr either.\n\n" +
+"See you soon space cowboy ~");
