9e184ac3d827140abcb40651c577694bfc8296f6
tiboitel <tiboitel@student.42.fr>
2023-08-29 14:00:41 +0200

    feat: In component home, addition of the behavior of displaying or hiding a section when clicking on a link of a section in the navigation bar.

--- src/ts/components/home.ts
+++ src/ts/components/home.ts
@@ -1,8 +1,46 @@
-console.log("CONTACT ME:\n" +
-"----------\n\n" +
-"(\\__/)\n" +
-"(o^-^)\n" +
-"(_('')(')\n\n" +
-"Hello ! If you like this portfolio, leave a star on the Github repo: https://github.com/tiboitel/portfolio/\n\n" +
-"Don't hesitate to PM me at tiboitel@student.42.fr either.\n\n" +
-"See you soon space cowboy ~");
+"use strict"
+
+class HomeComponent {
+    constructor() {
+        this.setNavigationLinksCallbacks();
+    }
+
+    private setSectionVisible(sectionName: string, isVisible: boolean): void {
+        const section = document.getElementById(sectionName);
+    
+        if (section !== null) {
+            if (isVisible) {
+                section.style.visibility = 'visible';
+                section.style.display = 'initial';
+            } else {
+                section.style.visibility = 'hidden';
+                section.style.display = 'none';
+            }
+        }
+    }
+
+    private setNavigationLinksCallbacks() {
+        const aboutLink = document.querySelector('a[data-id="link-about"]');
+        const projectsLink = document.querySelector('a[data-id="link-projects"]');
+        const contact = document.querySelector('a[data-id="link-contact"]');
+
+        if (aboutLink)
+            aboutLink.addEventListener('click', () => this.onNavigationLinkClicked('about'));
+        if (projectsLink)
+            projectsLink.addEventListener('click', () => this.onNavigationLinkClicked('projects'));
+        if (contact)
+            contact.addEventListener('click', () => this.onNavigationLinkClicked('contact'));
+    }
+
+    private onNavigationLinkClicked(sectionName :string) {
+        const sections = ['about', 'projects', 'contact'];
+
+        sections.forEach((value) => {
+            if (value == sectionName) {
+                this.setSectionVisible(value, true);
+            } else {
+                this.setSectionVisible(value, false);
+            }
+        });
+    }
+}
---
+++ src/ts/index.ts
@@ -0,0 +1,18 @@
+function welcome() {
+    console.log("CONTACT ME:\n" +
+        "----------\n\n" +
+        "(\\__/)\n" +
+        "(o^-^)\n" +
+        "(_('')(')\n\n" +
+        "Hello ! If you like this portfolio, leave a star on the Github repo: https://github.com/tiboitel/portfolio/\n\n" +
+        "Don't hesitate to PM me at tiboitel@student.42.fr either.\n\n" +
+        "See you soon space cowboy ~"
+    );
+}
+
+function main() {
+    const homeComponent = new HomeComponent();
+    welcome();
+}
+
+document.addEventListener('DOMContentLoaded', main);
